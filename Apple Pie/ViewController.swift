6de55c60da9dd8e8ea796800fe111b11475831f7
gross-micah <gross.micah@gmail.com>
2018-01-23 16:02:45 -0800

    hangman for ipad completed

--- Apple Pie/ViewController.swift
+++ Apple Pie/ViewController.swift
@@ -11,8 +11,16 @@ import UIKit
 class ViewController: UIViewController {
 	var listOfWords = ["pilfer", "enthusiasm", "clown", "shakshuka", "calvin", "hobbes", "peanuts"]
 	let incorrectMovesAllowed = 7
-	var totalWins = 0
-	var totalLosses = 0
+	var totalWins = 0 {
+		didSet {
+			newRound()
+		}
+	}
+	var totalLosses = 0 {
+		didSet {
+			newRound()
+		}
+	}
 	var currentGame: Game!
 	
 	override func viewDidLoad() {
@@ -21,13 +29,29 @@ class ViewController: UIViewController {
 	}
 	
 	func newRound() {
-		let newWord = listOfWords.removeFirst()
-		currentGame = Game(word: newWord, incorrectMovesRemaining: incorrectMovesAllowed, guessedLetters: [])
-		updateUI()
+		if !listOfWords.isEmpty {
+			let newWord = listOfWords.removeFirst()
+			currentGame = Game(word: newWord, incorrectMovesRemaining: incorrectMovesAllowed, guessedLetters: [])
+			enableLetterButtons(true)
+			updateUI()
+		} else {
+			enableLetterButtons(false)
+		}
+	}
+	
+	func enableLetterButtons(_ enable: Bool) {
+		for button in letterButtons {
+			button.isEnabled = enable
+		}
 	}
 	
 	func updateUI() {
-		correctWordLabel.text = currentGame.formattedWord
+		var letters = [String]()
+		for letter in currentGame.formattedWord {
+			letters.append(String(letter))
+		}
+		let wordWithSpacing = letters.joined(separator: " ")
+		correctWordLabel.text = wordWithSpacing
 		scoreLabel.text = "Wins: \(totalWins), Losses: \(totalLosses)"
 		treeImageView.image = UIImage(named: "Tree \(currentGame.incorrectMovesRemaining)")
 	}
@@ -42,8 +66,19 @@ class ViewController: UIViewController {
 		let letterString = sender.title(for: .normal)!
 		let letter = Character(letterString.lowercased())
 		currentGame.playerGuessed(letter: letter)
-		updateUI()
+		updateGameState()
 	}
+	
+	func updateGameState() {
+		if currentGame.incorrectMovesRemaining == 0 {
+			totalLosses += 1
+		} else if currentGame.word == currentGame.formattedWord {
+			totalWins += 1
+		} else {
+			updateUI()
+		}
+	}
+	
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Swift-Apple-Pie
 Apple Pie guided project from App Development With Swift
+
+Hangman formatted for iPads
